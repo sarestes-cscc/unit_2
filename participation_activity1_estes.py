30c4b8d753cb2408668773ae9dbddbbebd5e9fc9
Sarah Estes <sestes2@student.cscc.edu>
2026-09-09 23:10:04 -0400

    Added some print() testing

--- participation_activity1_estes.py
+++ participation_activity1_estes.py
@@ -6,3 +6,11 @@ my_dictionary = {
     "favorite planet": "saturn",
     "fun fact": "I just got an ice machine and I'm really excited about it.",
 }
+
+# check for errors
+
+print(my_dictionary)
+
+print(my_dictionary["name"].title())
+
+print(f"My favorite planet is {my_dictionary["favorite planet"].title()}.\n")
