6ccd79bfa47f6f24be73dc2f0bfcd6362432347b
Sarah Estes <sestes2@student.cscc.edu>
2026-09-09 22:54:23 -0400

    first commit

---
+++ participation_activity1_estes.py
@@ -0,0 +1,8 @@
+# simple dictionary for participation activity 1, CSCI 1511
+
+my_dictionary = {
+    "name": "sarah",
+    "favorite color": "purple",
+    "favorite planet": "saturn",
+    "fun fact": "I just got an ice machine and I'm really excited about it.",
+}
